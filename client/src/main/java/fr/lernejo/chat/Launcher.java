2f8a8122bed8c5bfb3dcfbadbf1a14b3c5d08e7e
Edwin LI <eli@et.esiea.fr>
2023-02-10 12:29:57 +0100

    Part 6&7

---
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -0,0 +1,53 @@
+package fr.lernejo.chat;
+
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Launcher {
+
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Launcher.class);
+        RabbitTemplate rabbitTemplate = context.getBean(RabbitTemplate.class);
+        System.out.println("Input a message, we will send it for you (q for quit)");
+        try (
+            Scanner scanner = new Scanner( System.in ) ) {
+
+            while( true ) {
+
+                String value = scanner.nextLine();
+
+
+                if (value.equals( "q" )) {
+                    System.out.println("bye");
+                    break;
+                }
+                else {
+                    rabbitTemplate.convertAndSend("","chat_messages",value);
+                    System.out.println("Message sent. Input a message, we will sent it for you");
+
+
+
+
+                }
+            }
+
+        }
+
+
+
+        //bean.run();
+        context.close();
+
+
+
+    }
+
+}
